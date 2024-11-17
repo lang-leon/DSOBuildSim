0fd7d615e055c1a07a80db25344bf4805cd24043
León Lang <leon.lang@stud-mail.uni-wuerzburg.de>
2024-11-17 03:49:09 +0100

    adjusted max enchantment values

--- src/main/java/kaukasus/Enums/EnchantTypeEnum.java
+++ src/main/java/kaukasus/Enums/EnchantTypeEnum.java
@@ -2,19 +2,19 @@ package kaukasus.Enums;
 
 public enum EnchantTypeEnum {
 
-    ANDERMAGIC_RESISTANCE("Andermagic Resistance", 0.2696),
-    ARMOR("Armor Value", 0.2706),
+    ANDERMAGIC_RESISTANCE("Andermagic Resistance", 0.26961),
+    ARMOR("Armor Value", 0.27058),
     ATTACK_SPEED("Attacks per Second", 0.47122),
-    BLOCK_VALUE("Block Value", 0.2432),
+    BLOCK_VALUE("Block Value", 0.24323),
     CRIT_VALUE("Critical Value", 0.24563),
     DAMAGE("Damage", 0.44776),
-    FIRE_RESISTANCE("Fire Resistance", 0.2696),
+    FIRE_RESISTANCE("Fire Resistance", 0.26961),
     HEALTH_POINTS("Health Points", 0.44537),
-    ICE_RESISTANCE("Ice Resistance", 0.2696),
-    LIGHTNING_RESISTANCE("Lightning Resistance", 0.2696),
-    MOVEMENT_SPEED("Movement Speed", 0.2245),
-    POISON_RESISTANCE("Poison Resistance", 0.2696),
-    RESISTANCE("All Resistance Values", 0.2749);
+    ICE_RESISTANCE("Ice Resistance", 0.26961),
+    LIGHTNING_RESISTANCE("Lightning Resistance", 0.26961),
+    MOVEMENT_SPEED("Movement Speed", 0.22450),
+    POISON_RESISTANCE("Poison Resistance", 0.26961),
+    RESISTANCE("All Resistance Values", 0.27489);
 
 
     private final String description;
