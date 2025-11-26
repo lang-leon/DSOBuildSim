8c90904547c7716c0e3728f3c383a394df76b4a9
León Lang <leon.lang@stud-mail.uni-wuerzburg.de>
2025-11-26 23:38:06 +0100

    vita mage test

--- src/test/java/CharacterTests/RealMageTest.java
+++ src/test/java/CharacterTests/RealMageTest.java
@@ -26,7 +26,7 @@ import java.util.*;
 
 public class RealMageTest {
     public static void main(String[] args) {
-        Character mage = pveNormal2();
+        Character mage = zohan();
         mage.calculateFinalStats();
         printStats(mage);
     }
@@ -448,6 +448,205 @@ public class RealMageTest {
         mage.setEssence(1.00);
 
         Pet mortisPet = new Pet("Mortis Doll", Map.of(OverallRelativeBuffTypeEnum.DAMAGE, 0.3, OverallRelativeBuffTypeEnum.ATTACK_SPEED, 0.3, OverallRelativeBuffTypeEnum.HEALTH_POINTS, 0.175), "");
+        mage.setPet(mortisPet);
+        mage.setCollectorBagBuff(Map.of(OverallRelativeBuffTypeEnum.HEALTH_POINTS, 0.33, OverallRelativeBuffTypeEnum.RESISTANCE, 0.15, OverallRelativeBuffTypeEnum.DAMAGE, 0.06, OverallRelativeBuffTypeEnum.BLOCK_VALUE, 0.02));
+
+        Tonic tonic = new Tonic("Tonic of Vigor", AbsoluteStatTypeEnum.HEALTH_POINTS, 156068.0);
+        //mage.setTonic(tonic);
+        Physic physic = new Physic("Advanced Physic of Vigor", OverallRelativeBuffTypeEnum.HEALTH_POINTS, 0.18);
+        //mage.setPhysic(physic);
+
+        return mage;
+    }
+
+    public static Character zohan() {
+        //2p destructor, sargon gloves, 2p seeker, q8, q9
+        Character mage = new Character(CharacterClassEnum.SPELLWEAVER, "2p destructor, sargon gloves, 2p seeker, q8, q9");
+
+        AbstractItem amulet = SpellweaverUniqueItems.AMULET_OF_THE_KRAKEN.getItem();
+        amulet.setEnchants(EnchantTypeEnum.HEALTH_POINTS);
+        amulet.setGems(AbsoluteStatTypeEnum.HEALTH_POINTS);
+        mage.addItem(amulet, ItemSlotEnum.AMULET);
+
+        AbstractItem cloak = SpellweaverMythicItem.ANCESTRAL_GLORY_CLOAK.getItem();
+        cloak.setEnchants(EnchantTypeEnum.DAMAGE);
+        cloak.setGems(AbsoluteStatTypeEnum.CRIT_VALUE);
+        mage.addItem(cloak, ItemSlotEnum.CLOAK);
+
+        AbstractItem belt = SpellweaverUniqueItems.BELT_OF_ZEAL.getItem();
+        belt.setEnchants(EnchantTypeEnum.DAMAGE);
+        belt.setGems(AbsoluteStatTypeEnum.DAMAGE);
+        mage.addItem(belt, ItemSlotEnum.BELT);
+
+        AbstractItem ring1 = SpellweaverMythicItem.RING_OF_ANCESTRAL_GLORY.getItem();
+        ring1.setEnchants(EnchantTypeEnum.DAMAGE);
+        ring1.setGems(AbsoluteStatTypeEnum.DAMAGE);
+        mage.addItem(ring1, ItemSlotEnum.RING1);
+
+        AbstractItem ring2 = SpellweaverMythicItem.RING_OF_ANCESTRAL_GLORY.getItem();
+        ring2.setEnchants(EnchantTypeEnum.DAMAGE);
+        Opal opal = new Opal(new Gem(AbsoluteStatTypeEnum.CRIT_VALUE, 2520.0), new Gem(AbsoluteStatTypeEnum.DAMAGE, 700.0), new Gem(AbsoluteStatTypeEnum.MOVEMENT_SPEED, 0.17));
+        ring2.addGems(opal, 10);
+        mage.addItem(ring2, ItemSlotEnum.RING2);
+
+        AbstractItem helmet = SpellweaverSetItems.MIGHTY_WRATHFUL_SEEKERS_HELMET.getItem();
+        helmet.setEnchants(EnchantTypeEnum.HEALTH_POINTS);
+        helmet.setGems(AbsoluteStatTypeEnum.HEALTH_POINTS);
+        mage.addItem(helmet, ItemSlotEnum.HELMET);
+
+        AbstractItem shoulders = SpellweaverSetItems.AUTOMATED_SHOULDERGUARDS.getItem();
+        shoulders.setEnchants(EnchantTypeEnum.HEALTH_POINTS);
+        shoulders.setGems(AbsoluteStatTypeEnum.HEALTH_POINTS);
+        mage.addItem(shoulders, ItemSlotEnum.SHOULDERS);
+
+        AbstractItem torso = SpellweaverSetItems.AUTOMATED_STERNUM_GUARD.getItem();
+        torso.setEnchants(EnchantTypeEnum.DAMAGE);
+        torso.setGems(AbsoluteStatTypeEnum.CRIT_VALUE);
+        mage.addItem(torso, ItemSlotEnum.TORSO);
+
+        AbstractItem gloves = SpellweaverSetItems.MIGHTY_WRATHFUL_SEEKERS_GLOVES.getItem();
+        gloves.setEnchants(EnchantTypeEnum.ATTACK_SPEED);
+        gloves.setGems(AbsoluteStatTypeEnum.ATTACK_SPEED);
+        mage.addItem(gloves, ItemSlotEnum.GLOVES);
+
+        AbstractItem boots = SpellweaverSetItems.MIGHTY_WRATHFUL_SEEKERS_BOOTS.getItem();
+        boots.setEnchants(EnchantTypeEnum.DAMAGE);
+        boots.addGems(AbsoluteStatTypeEnum.CRIT_VALUE, 8);
+        boots.addGems(AbsoluteStatTypeEnum.MOVEMENT_SPEED, 2);
+        mage.addItem(boots, ItemSlotEnum.BOOTS);
+
+        AbstractItem adornment = SpellweaverUniqueItems.STINGER.getItem();
+        adornment.setEnchants(EnchantTypeEnum.DAMAGE);
+        adornment.setGems(AbsoluteStatTypeEnum.DAMAGE);
+        mage.addItem(adornment, ItemSlotEnum.WEAPON_ADORNMENT);
+
+        AbstractItem twoHand = SpellweaverUniqueItems.GRIMACE_OF_THE_THUNDER_BEAST.getItem();
+        twoHand.setEnchants(EnchantTypeEnum.DAMAGE);
+        twoHand.setGems(AbsoluteStatTypeEnum.DAMAGE);
+        mage.addItem(twoHand, ItemSlotEnum.TWO_HAND_WEAPON);
+
+
+
+        RuneTrinket runeTrinket1 = new RuneTrinket();
+        runeTrinket1.addRune(new Rune("Rune of Vigor", OverallRelativeBuffTypeEnum.DAMAGE, 0.065));
+        runeTrinket1.addRune(new Rune("Rune of Vigor", OverallRelativeBuffTypeEnum.DAMAGE, 0.065));
+        runeTrinket1.addRune(new Rune("Rune of Vigor", OverallRelativeBuffTypeEnum.DAMAGE, 0.065));
+        runeTrinket1.addRune(new Rune("Rune of Vigor", OverallRelativeBuffTypeEnum.DAMAGE, 0.065));
+        runeTrinket1.addRune(new Rune("Rune of Vigor", OverallRelativeBuffTypeEnum.DAMAGE, 0.065));
+        runeTrinket1.addRune(new Rune("Rune of Devastation", OverallRelativeBuffTypeEnum.CRIT_VALUE, 0.065));
+        runeTrinket1.addRune(new Rune("Rune of Devastation", OverallRelativeBuffTypeEnum.CRIT_VALUE, 0.065));
+        runeTrinket1.addRune(new Rune("Rune of Devastation", OverallRelativeBuffTypeEnum.CRIT_VALUE, 0.065));
+        runeTrinket1.addRune(new Rune("Rune of Devastation", OverallRelativeBuffTypeEnum.CRIT_VALUE, 0.065));
+        runeTrinket1.addRune(new Rune("Rune of Devastation", OverallRelativeBuffTypeEnum.CRIT_VALUE, 0.065));
+        mage.addRuneTrinket(runeTrinket1, 1);
+
+        RuneTrinket runeTrinket2 = new RuneTrinket();
+        mage.addRuneTrinket(runeTrinket2, 2);
+
+        RuneTrinket runeTrinket3 = new RuneTrinket();
+        runeTrinket3.addRune(new SpecialRune("Concentrated Solstice Rune", Map.of(OverallRelativeBuffTypeEnum.ICE_RESISTANCE, 0.065, OverallRelativeBuffTypeEnum.HEALTH_POINTS, 0.065, OverallRelativeBuffTypeEnum.CRIT_VALUE, 0.065), ""));
+        runeTrinket3.addRune(new SpecialRune("Concentrated Solstice Rune", Map.of(OverallRelativeBuffTypeEnum.ICE_RESISTANCE, 0.065, OverallRelativeBuffTypeEnum.HEALTH_POINTS, 0.065, OverallRelativeBuffTypeEnum.CRIT_VALUE, 0.065), ""));
+        runeTrinket3.addRune(new SpecialRune("Concentrated Solstice Rune", Map.of(OverallRelativeBuffTypeEnum.ICE_RESISTANCE, 0.065, OverallRelativeBuffTypeEnum.HEALTH_POINTS, 0.065, OverallRelativeBuffTypeEnum.CRIT_VALUE, 0.065), ""));
+        runeTrinket3.addRune(new SpecialRune("Concentrated Solstice Rune", Map.of(OverallRelativeBuffTypeEnum.ICE_RESISTANCE, 0.065, OverallRelativeBuffTypeEnum.HEALTH_POINTS, 0.065, OverallRelativeBuffTypeEnum.CRIT_VALUE, 0.065), ""));
+        runeTrinket3.addRune(new SpecialRune("Concentrated Solstice Rune", Map.of(OverallRelativeBuffTypeEnum.ICE_RESISTANCE, 0.065, OverallRelativeBuffTypeEnum.HEALTH_POINTS, 0.065, OverallRelativeBuffTypeEnum.CRIT_VALUE, 0.065), ""));
+        mage.addRuneTrinket(runeTrinket3, 3);
+
+        RuneTrinket runeTrinket4 = new RuneTrinket();
+        runeTrinket4.addRune(new Rune("Rune of Celerity", OverallRelativeBuffTypeEnum.ATTACK_SPEED, 0.065));
+        runeTrinket4.addRune(new Rune("Rune of Celerity", OverallRelativeBuffTypeEnum.ATTACK_SPEED, 0.065));
+        runeTrinket4.addRune(new Rune("Rune of Celerity", OverallRelativeBuffTypeEnum.ATTACK_SPEED, 0.065));
+        runeTrinket4.addRune(new Rune("Rune of Celerity", OverallRelativeBuffTypeEnum.ATTACK_SPEED, 0.065));
+        runeTrinket4.addRune(new Rune("Rune of Celerity", OverallRelativeBuffTypeEnum.ATTACK_SPEED, 0.065));
+        runeTrinket4.addRune(new Rune("Rune of Vitality", OverallRelativeBuffTypeEnum.HEALTH_POINTS, 0.065));
+        runeTrinket4.addRune(new Rune("Rune of Vitality", OverallRelativeBuffTypeEnum.HEALTH_POINTS, 0.065));
+        runeTrinket4.addRune(new Rune("Rune of Vitality", OverallRelativeBuffTypeEnum.HEALTH_POINTS, 0.065));
+        runeTrinket4.addRune(new Rune("Rune of Vitality", OverallRelativeBuffTypeEnum.HEALTH_POINTS, 0.065));
+        runeTrinket4.addRune(new Rune("Rune of Vitality", OverallRelativeBuffTypeEnum.HEALTH_POINTS, 0.065));
+        mage.addRuneTrinket(runeTrinket4, 4);
+
+        RuneTrinket runeTrinket5 = new RuneTrinket();
+        runeTrinket5.addRune(new Rune("Rune of Fortitude", OverallRelativeBuffTypeEnum.ARMOR, 0.065));
+        runeTrinket5.addRune(new Rune("Rune of Fortitude", OverallRelativeBuffTypeEnum.ARMOR, 0.065));
+        //runeTrinket5.addRune(new SpecialRune("Rune of Rising Vigor", Map.of(OverallRelativeBuffTypeEnum.MOVEMENT_SPEED, 0.10, OverallRelativeBuffTypeEnum.ATTACK_SPEED, 0.06), ""));
+        runeTrinket5.addRune(new SpecialRune("Rune of the Realm Changer", Map.of(), "Increases the Realm Changer talent by 5."));
+        runeTrinket5.addRune(new SpecialRune("Rune of the Andermant Fever", Map.of(), "Increases the Andermant Fever talent by 5."));
+        runeTrinket5.addRune(new Rune("Rune of Acceleration", OverallRelativeBuffTypeEnum.MOVEMENT_SPEED, 0.065));
+        runeTrinket5.addRune(new Rune("Rune of Acceleration", OverallRelativeBuffTypeEnum.MOVEMENT_SPEED, 0.065));
+        runeTrinket5.addRune(new Rune("Rune of Acceleration", OverallRelativeBuffTypeEnum.MOVEMENT_SPEED, 0.065));
+        runeTrinket5.addRune(new Rune("Rune of Acceleration", OverallRelativeBuffTypeEnum.MOVEMENT_SPEED, 0.065));
+        mage.addRuneTrinket(runeTrinket5, 5);
+
+        JewelTrinket jewelTrinket1 = new JewelTrinket();
+        jewelTrinket1.addJewel(new Jewel("Jewel of Eternal Scorn", OverallRelativeBuffTypeEnum.CRIT_VALUE, 0.15, "..."));
+        jewelTrinket1.addJewel(new Jewel("Jewel of Rage", "..."));
+        jewelTrinket1.addJewel(new Jewel("Jewel of Amplified Healing", "..."));
+        jewelTrinket1.addJewel(new Jewel("Jewel of the Frozen Heart", "..."));
+        jewelTrinket1.addJewel(new Jewel("Jewel of the Easter Fever", "..."));
+        jewelTrinket1.addJewel(new Jewel("Jewel of the Gem Fortune", "..."));
+        jewelTrinket1.addJewel(new Jewel("Jewel of the Eternal Wrath", "..."));
+        jewelTrinket1.addJewel(new Jewel("Jewel of the Ingredient Hunter", "..."));
+        jewelTrinket1.addJewel(new Jewel("Jewel of the Ingredient Hunter", "..."));
+        jewelTrinket1.addJewel(new Jewel("Jewel of the Ingredient Hunter", "..."));
+        mage.addJewelTrinket(jewelTrinket1, 1);
+
+        JewelTrinket jewelTrinket2 = new JewelTrinket();
+        jewelTrinket2.addJewel(new Jewel("Jewel of Focus", "..."));
+        jewelTrinket2.addJewel(new Jewel("Jewel of Focus", "..."));
+        jewelTrinket2.addJewel(new Jewel("Jewel of Focus", "..."));
+        jewelTrinket2.addJewel(new Jewel("Jewel of Focus", "..."));
+        jewelTrinket2.addJewel(new Jewel("Jewel of Focus", "..."));
+        jewelTrinket2.addJewel(new Jewel("Jewel of Vigor", OverallRelativeBuffTypeEnum.DAMAGE, 0.1));
+        jewelTrinket2.addJewel(new Jewel("Jewel of Ambidextrous Vigor", OverallRelativeBuffTypeEnum.TWO_HAND_DAMAGE, 0.1));
+        jewelTrinket2.addJewel(new Jewel("Jewel of Vitality", OverallRelativeBuffTypeEnum.HEALTH_POINTS, 0.1));
+        jewelTrinket2.addJewel(new Jewel("Jewel of Encouragement", "..."));
+        jewelTrinket2.addJewel(new Jewel("Jewel of Contribution", "..."));
+        mage.addJewelTrinket(jewelTrinket2, 2);
+
+        JewelTrinket jewelTrinket3 = new JewelTrinket();
+        jewelTrinket3.addJewel(new Jewel("Jewel of Lasting Health", "..."));
+        jewelTrinket3.addJewel(new Jewel("Jewel of Converse", "..."));
+        jewelTrinket3.addJewel(new Jewel("Fiery Flower Jewel", "..."));
+        jewelTrinket3.addJewel(new Jewel("Jewel of Strenuousness", "..."));
+        jewelTrinket3.addJewel(new Jewel("Jewel of Fortitude", OverallRelativeBuffTypeEnum.ARMOR, 0.1));
+        jewelTrinket3.addJewel(new Jewel("Jewel of Prolongation", "..."));
+        jewelTrinket3.addJewel(new Jewel("Jewel of Revival Boon", "..."));
+        jewelTrinket3.addJewel(new Jewel("Jewel of Pent-up-Power", "..."));
+        jewelTrinket3.addJewel(new Jewel("Jewel of Scorching Ray", "..."));
+        mage.addJewelTrinket(jewelTrinket3, 3);
+
+        WisdomSkillTree wisdomSkillTree = new WisdomSkillTree();
+        wisdomSkillTree.setLevel(60, 1,1);
+        wisdomSkillTree.setLevel(60, 1,2);
+
+        wisdomSkillTree.setLevel(80, 2,1);
+        wisdomSkillTree.setLevel(80, 2,2);
+        wisdomSkillTree.setLevel(40, 2,3);
+
+        wisdomSkillTree.setLevel(40, 3,1);
+        wisdomSkillTree.setLevel(80, 3,2);
+        wisdomSkillTree.setLevel(80, 3,3);
+
+        wisdomSkillTree.setLevel(1, 4,1);
+
+        wisdomSkillTree.setLevel(60, 5,1);
+        wisdomSkillTree.setLevel(60, 5,2);
+
+        wisdomSkillTree.setLevel(60, 6,1);
+        wisdomSkillTree.setLevel(60, 6,2);
+        wisdomSkillTree.setLevel(30, 6,3);
+
+        wisdomSkillTree.setLevel(15, 7,1);
+        wisdomSkillTree.setLevel(1, 7,2);
+        wisdomSkillTree.setLevel(1, 7,3);
+
+        wisdomSkillTree.setLevel(15, 8,1);
+        wisdomSkillTree.setLevel(15, 8,2);
+
+        mage.setWisdomSkillTree(wisdomSkillTree);
+
+        mage.setEssence(1.00);
+
+        Pet mortisPet = new Pet("Mortis Doll", Map.of(OverallRelativeBuffTypeEnum.DAMAGE, 0.3, OverallRelativeBuffTypeEnum.CRIT_VALUE, 0.3, OverallRelativeBuffTypeEnum.HEALTH_POINTS, 0.175), "");
         mage.setPet(mortisPet);
         mage.setCollectorBagBuff(Map.of(OverallRelativeBuffTypeEnum.HEALTH_POINTS, 0.33, OverallRelativeBuffTypeEnum.RESISTANCE, 0.15, OverallRelativeBuffTypeEnum.DAMAGE, 0.06, OverallRelativeBuffTypeEnum.BLOCK_VALUE, 0.02));
 
