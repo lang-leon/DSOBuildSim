9ad6fa162e752e8e674e3261a9bd63880fdd310d
lang-leon <leon.lang1602@gmail.com>
2025-12-21 13:41:41 +0100

    added all needed pet classes

--- src/main/java/com/langleon/dsobuildsim/pets/PetConfig.java
+++ src/main/java/com/langleon/dsobuildsim/pets/PetConfig.java
@@ -1,5 +1,9 @@
 package com.langleon.dsobuildsim.pets;
 
-public class PetConfig
+import com.langleon.dsobuildsim.enums.pets.PetType;
+
+import java.util.Map;
+
+public record PetConfig(Map<PetType, PetDefinition> pets, Map<Integer, Integer> normalUpgradeCosts, Map<Integer, Integer> silverCatUpgradeCosts, Map<Integer, Integer> gildedCatUpgradeCosts)
 {
 }
--- src/main/java/com/langleon/dsobuildsim/pets/PetFactory.java
+++ src/main/java/com/langleon/dsobuildsim/pets/PetFactory.java
@@ -1,4 +1,40 @@
 package com.langleon.dsobuildsim.pets;
 
+import com.langleon.dsobuildsim.enums.OverallRelativeBuffType;
+import com.langleon.dsobuildsim.enums.pets.PetType;
+import java.util.Map;
+
 public class PetFactory {
+    private final PetConfig config;
+
+    public PetFactory(PetConfig config) {
+        this.config = config;
+    }
+
+    public int getUpgradeCost(Pet pet)
+    {
+        switch (pet.getPetUpgradeType())
+        {
+            case NORMAL -> {
+                return this.config.normalUpgradeCosts().get(pet.getTier());
+            }
+            case SILVERCAT -> {
+                return this.config.silverCatUpgradeCosts().get(pet.getTier());
+            }
+            case GILDEDCAT -> {
+                return this.config.gildedCatUpgradeCosts().get(pet.getTier());
+            }
+            default -> {
+                return -1;
+            }
+        }
+    }
+
+    public Pet createPet(PetType petType, int tier)
+    {
+        PetDefinition petDefinition = this.config.pets().get(petType);
+        Map<OverallRelativeBuffType, Double> stats = petDefinition.statsPerTier().get(tier);
+        if (stats == null) throw new IllegalArgumentException("Invalid pet tier: " + tier + "!");
+        return new Pet(petType, petDefinition.petUpgradeType(), tier, stats, petDefinition.description());
+    }
 }
