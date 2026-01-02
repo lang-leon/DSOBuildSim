f997a58efbb7f4269ba12c69be3ea3075064a29e
lang-leon <leon.lang1602@gmail.com>
2026-01-02 02:17:52 +0100

    added/fixed toString methods

--- src/main/java/com/langleon/dsobuildsim/enums/StatType.java
+++ src/main/java/com/langleon/dsobuildsim/enums/StatType.java
@@ -32,13 +32,18 @@ public enum StatType {
     XP_GAIN("XP Gain");
 
 
-    private final String description;
+    private final String name;
 
     StatType(String description){
-        this.description = description;
+        this.name = description;
     }
 
-    public String getDescription() {
-        return description;
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
     }
 }
