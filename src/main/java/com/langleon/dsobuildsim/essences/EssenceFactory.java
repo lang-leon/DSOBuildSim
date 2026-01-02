aa12ca181de9eb2f6656d2fd0bb1fac87f9885a9
lang-leon <leon.lang1602@gmail.com>
2026-01-02 18:22:13 +0100

    completed all classes/files needed for essences

--- src/main/java/com/langleon/dsobuildsim/essences/EssenceFactory.java
+++ src/main/java/com/langleon/dsobuildsim/essences/EssenceFactory.java
@@ -1,4 +1,24 @@
 package com.langleon.dsobuildsim.essences;
 
+import com.langleon.dsobuildsim.enums.essences.EssenceType;
+
 public class EssenceFactory {
+    private final EssenceConfig config;
+
+    public EssenceFactory(EssenceConfig config) {
+        this.config = config;
+    }
+
+    public Essence createEssence(EssenceType essenceType, int tier){
+        EssenceDefinition essenceDefinition = config.essences().get(essenceType);
+        if (!essenceDefinition.damagePerTier().containsKey(tier)) throw new IllegalArgumentException("Invalid pet tier: " + tier + "!");
+        return new Essence(essenceType, tier, essenceDefinition.damagePerTier().get(tier), essenceDefinition.descriptionPerTier().get(tier));
+    }
+
+    public Essence createEssence(EssenceType essenceType){
+        EssenceDefinition essenceDefinition = config.essences().get(essenceType);
+        int tier = essenceDefinition.defaultTier();
+        if (!essenceDefinition.damagePerTier().containsKey(tier)) throw new IllegalArgumentException("Invalid pet tier: " + tier + "!");
+        return new Essence(essenceType, tier, essenceDefinition.damagePerTier().get(tier), essenceDefinition.descriptionPerTier().get(tier));
+    }
 }
