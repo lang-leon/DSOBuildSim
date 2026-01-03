1a1498459eeb85e6d49040e5a7c3bffa32df951a
lang-leon <leon.lang1602@gmail.com>
2026-01-03 01:58:09 +0100

    converted DragonStone.java to record added test cases for DragonStoneFactory

--- src/main/java/com/langleon/dsobuildsim/dragonstones/DragonStone.java
+++ src/main/java/com/langleon/dsobuildsim/dragonstones/DragonStone.java
@@ -5,32 +5,5 @@ import com.langleon.dsobuildsim.enums.dragonstones.DragonStoneType;
 
 import java.util.Map;
 
-public class DragonStone {
-    private final DragonStoneType dragonStoneType;
-    private final int tier;
-    private final Map<StatType, Double> stats;
-    private final String description;
-
-    public DragonStone(DragonStoneType dragonStoneType, int tier, Map<StatType, Double> stats, String description) {
-        this.dragonStoneType = dragonStoneType;
-        this.tier = tier;
-        this.stats = stats;
-        this.description = description;
-    }
-
-    public DragonStoneType getDragonStoneType() {
-        return dragonStoneType;
-    }
-
-    public int getTier() {
-        return tier;
-    }
-
-    public Map<StatType, Double> getStats() {
-        return stats;
-    }
-
-    public String getDescription() {
-        return description;
-    }
+public record DragonStone(DragonStoneType dragonStoneType, int tier, Map<StatType, Double> stats, String description) {
 }
