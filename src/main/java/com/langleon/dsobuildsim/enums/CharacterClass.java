d81cdadc751b0c7ca4a12f4dc5a0421f9b021da5
lang-leon <leon.lang1602@gmail.com>
2026-03-04 17:35:46 +0100

    Add missing default stats

--- src/main/java/com/langleon/dsobuildsim/enums/CharacterClass.java
+++ src/main/java/com/langleon/dsobuildsim/enums/CharacterClass.java
@@ -1,13 +1,22 @@
 package com.langleon.dsobuildsim.enums;
 
 import java.util.Map;
+import java.util.concurrent.StructuredTaskScope;
 
 public enum CharacterClass {
 
-    DRAGONKNIGHT("Dragonknight", Map.of(StatType.DAMAGE, 16800.0, StatType.MANA, 100.0, StatType.MANA_PER_SECOND, 0.0, StatType.HEALTH_POINTS, 450000.0), Map.of(StatType.ATTACK_SPEED, 0.50)),
-    RANGER("Ranger", Map.of(StatType.DAMAGE, 29400.0, StatType.MANA, 100.0, StatType.MANA_PER_SECOND, 8.0, StatType.HEALTH_POINTS, 345000.0), Map.of()),
-    SPELLWEAVER("Spellweaver", Map.of(StatType.DAMAGE, 50400.0, StatType.MANA, 100.0, StatType.MANA_PER_SECOND, 12.0, StatType.HEALTH_POINTS, 150000.0), Map.of(StatType.DAMAGE, 0.50)),
-    STEAM_MECHANICUS("Steam Mechanicus", Map.of(StatType.DAMAGE, 38640.0, StatType.MANA, 100.0, StatType.MANA_PER_SECOND, 3.0, StatType.HEALTH_POINTS, 262500.0), Map.of());
+    DRAGONKNIGHT("Dragonknight",
+            Map.of(StatType.DAMAGE, 16800.0, StatType.ATTACK_SPEED, 1.0, StatType.MANA, 100.0, StatType.MANA_PER_SECOND, 0.0, StatType.MOVEMENT_SPEED, 5.0, StatType.HEALTH_POINTS, 450000.0),
+            Map.of(StatType.ATTACK_SPEED, 0.50, StatType.HONOR_GAIN, 1.00, StatType.XP_GAIN, 1.00)),
+    RANGER("Ranger",
+            Map.of(StatType.DAMAGE, 29400.0, StatType.ATTACK_SPEED, 1.0, StatType.MANA, 100.0, StatType.MANA_PER_SECOND, 8.0, StatType.MOVEMENT_SPEED, 5.0, StatType.HEALTH_POINTS, 345000.0),
+            Map.of(StatType.HONOR_GAIN, 1.00, StatType.XP_GAIN, 1.00)),
+    SPELLWEAVER("Spellweaver",
+            Map.of(StatType.DAMAGE, 50400.0, StatType.ATTACK_SPEED, 1.0, StatType.MANA, 100.0, StatType.MANA_PER_SECOND, 12.0, StatType.MOVEMENT_SPEED, 5.0, StatType.HEALTH_POINTS, 150000.0),
+            Map.of(StatType.DAMAGE, 0.50, StatType.HONOR_GAIN, 1.00, StatType.XP_GAIN, 1.00)),
+    STEAM_MECHANICUS("Steam Mechanicus",
+            Map.of(StatType.DAMAGE, 38640.0, StatType.ATTACK_SPEED, 1.0, StatType.MANA, 100.0, StatType.MANA_PER_SECOND, 3.0, StatType.MOVEMENT_SPEED, 5.0, StatType.HEALTH_POINTS, 262500.0),
+            Map.of(StatType.HONOR_GAIN, 1.00, StatType.XP_GAIN, 1.00));
 
     private final String className;
     private final Map<StatType, Double> classBaseStats;
