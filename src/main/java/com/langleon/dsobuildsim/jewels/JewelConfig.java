b06f00451de5eaba921713048940cf538c8a8472
lang-leon <leon.lang1602@gmail.com>
2026-03-09 00:33:47 +0100

    - Split stored jewel data into separate JSON files by class - Update JewelFactory to load jewels from new structure - Refactor for modularity and easier future expansions

--- src/main/java/com/langleon/dsobuildsim/jewels/JewelConfig.java
+++ src/main/java/com/langleon/dsobuildsim/jewels/JewelConfig.java
@@ -2,6 +2,6 @@ package com.langleon.dsobuildsim.jewels;
 
 import java.util.Map;
 
-public record JewelConfig (Map<JewelType, JewelDefinition> jewels, Map<Integer, Integer> upgradeCosts)
+public record JewelConfig (Map<JewelType, JewelDefinition> spellweaverJewels, Map<JewelType, JewelDefinition> dragonknightJewels, Map<JewelType, JewelDefinition> rangerJewels, Map<JewelType, JewelDefinition> steamMechanicusJewels, Map<Integer, Integer> upgradeCosts)
 {
 }
