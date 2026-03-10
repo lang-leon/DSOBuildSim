3e0dc5fa7337a28f45d60431ed006e8ac8809470
lang-leon <leon.lang1602@gmail.com>
2026-03-10 18:22:54 +0100

    Add test cases for DragonStoneMapper and DragonStoneResolver

---
+++ src/test/java/com/langleon/dsobuildsim/mapper/DragonStoneMapperTest.java
@@ -0,0 +1,48 @@
+package com.langleon.dsobuildsim.mapper;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.langleon.dsobuildsim.common.StatType;
+import com.langleon.dsobuildsim.dragonstones.DragonStoneConfig;
+import com.langleon.dsobuildsim.dragonstones.DragonStoneType;
+import com.langleon.dsobuildsim.dto.DragonStoneDTO;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Map;
+import java.util.Objects;
+
+public class DragonStoneMapperTest {
+
+    private DragonStoneConfig config;
+
+    @BeforeEach
+    void setup() throws IOException {
+        try (var reader = new InputStreamReader(Objects.requireNonNull(getClass().getResourceAsStream("/data/dragonstones.json"))))
+        {
+            ObjectMapper objectMapper = new ObjectMapper();
+            config = objectMapper.readValue(reader, DragonStoneConfig.class);
+        }
+    }
+
+    @Test
+    void shouldMapDragonStoneDefinitionToDragonStoneDTO()
+    {
+        DragonStoneDTO dto = DragonStoneMapper.from(config.dragonStones().get(DragonStoneType.POWERSTONE));
+
+        Map<Integer, Map<StatType, Double>> statsPerTier = Map.of(
+                3, Map.of(StatType.HEALTH_POINTS, 0.025),
+                4, Map.of(StatType.HEALTH_POINTS, 0.05),
+                5, Map.of(StatType.HEALTH_POINTS, 0.075)
+        );
+        Map<Integer, String> descriptionPerTier = Map.of(3, "+ 2.50% Health Points", 4, "+ 5.00% Health Points", 5, "+ 7.50% Health Points");
+
+        Assertions.assertEquals("POWERSTONE", dto.dragonStoneType());
+        Assertions.assertEquals("Powerstone", dto.name());
+        Assertions.assertEquals(5, dto.tier());
+        Assertions.assertEquals(statsPerTier, dto.stats());
+        Assertions.assertEquals(descriptionPerTier, dto.description());
+    }
+}
