7d00706fc8a0a28549c54a0634f233ef3edb7dce
lang-leon <leon.lang1602@gmail.com>
2026-03-12 06:39:57 +0100

    Add GameDataConfig, GameDataMapper and ItemDefinitionMapper

--- src/main/java/com/langleon/dsobuildsim/config/ClassStatsConfig.java
+++ src/main/java/com/langleon/dsobuildsim/config/ClassStatsConfig.java
@@ -1,5 +1,6 @@
-package com.langleon.dsobuildsim.character;
+package com.langleon.dsobuildsim.config;
 
+import com.langleon.dsobuildsim.character.CharacterClass;
 import com.langleon.dsobuildsim.dto.ClassStatsDTO;
 
 import java.util.Map;
