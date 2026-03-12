fa6dca4532088cf077cf2646c82e5e6e75118fee
lang-leon <leon.lang1602@gmail.com>
2026-03-12 04:57:23 +0100

    Add ItemDTO, ItemDefinitionDTO as well as mapper and resolver

---
+++ src/main/java/com/langleon/dsobuildsim/dto/ItemDefinitionDTO.java
@@ -0,0 +1,30 @@
+package com.langleon.dsobuildsim.dto;
+
+import com.langleon.dsobuildsim.common.StatType;
+import com.langleon.dsobuildsim.items.core.ItemType;
+import com.langleon.dsobuildsim.items.core.enums.ItemCategory;
+import com.langleon.dsobuildsim.items.core.enums.ItemSlotType;
+import com.langleon.dsobuildsim.sets.SetType;
+
+import java.util.List;
+import java.util.Map;
+
+public record ItemDefinitionDTO(
+        ItemCategory itemCategory,
+        ItemType itemType,
+        String name,
+        int defaultLevel,
+        int tier,
+        ItemSlotType itemSlotType,
+        Map<StatType, Double> rawBaseValues,
+
+        //optional fields depending on category
+        Map<StatType, Double> uniqueRelativeValues,
+        Map<StatType, Double> uniqueAbsoluteValues,
+        Map<StatType, Double> uniqueBaseValues,
+        List<EnchantmentDTO> uniqueEnchantments,
+        String uniqueDescription,
+
+        SetType set
+        ) {
+}
---
+++ src/main/java/com/langleon/dsobuildsim/mapper/ItemDefinitionMapper.java
@@ -0,0 +1,4 @@
+package com.langleon.dsobuildsim.mapper;
+
+public class ItemDefinitionMapper {
+}
