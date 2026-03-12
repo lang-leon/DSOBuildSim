4b3ffc10600dbe4b71b0435dca2b4d920eae30ce
lang-leon <leon.lang1602@gmail.com>
2026-03-12 05:09:10 +0100

    Moved gem limit check to equipItem method

--- src/test/java/com/langleon/dsobuildsim/character/CharacterTest.java
+++ src/test/java/com/langleon/dsobuildsim/character/CharacterTest.java
@@ -46,11 +46,9 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
-import java.util.zip.CRC32;
 
 public class CharacterTest {
 
