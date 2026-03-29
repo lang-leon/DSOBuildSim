a96ee3a9f830cbe6201274b4875de2fd8a946564
lang-leon <leon.lang1602@gmail.com>
2026-03-30 01:43:35 +0200

    Introduce Collector Bag Bonus system.

---
+++ src/main/java/com/langleon/dsobuildsim/collectorbagbonus/enums/CollectorBagBonusType.java
@@ -0,0 +1,12 @@
+package com.langleon.dsobuildsim.collectorbagbonus.enums;
+
+public enum CollectorBagBonusType {
+    BULWARK_OF_SIX(),
+    RESISTANCE_OF_SIX(),
+    SOULBOUND_POWER_1(),
+    SOULBOUND_POWER_2(),
+    STRENGTH_OF_NINE(),
+    TRIAD_OF_LIFE(),
+    WAVING_OF_FORTUNE(),
+    ;
+}
