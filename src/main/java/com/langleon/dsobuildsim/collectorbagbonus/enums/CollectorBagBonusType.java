75866dd0f44052caea8260884aa572a21e9e7224
lang-leon <leon.lang1602@gmail.com>
2026-03-30 15:03:17 +0200

    Introduce test cases for CollectorBagMapper and CollectorBagFactory

--- src/main/java/com/langleon/dsobuildsim/collectorbagbonus/enums/CollectorBagBonusType.java
+++ src/main/java/com/langleon/dsobuildsim/collectorbagbonus/enums/CollectorBagBonusType.java
@@ -1,12 +1,22 @@
 package com.langleon.dsobuildsim.collectorbagbonus.enums;
 
 public enum CollectorBagBonusType {
-    BULWARK_OF_SIX(),
-    RESISTANCE_OF_SIX(),
-    SOULBOUND_POWER_1(),
-    SOULBOUND_POWER_2(),
-    STRENGTH_OF_NINE(),
-    TRIAD_OF_LIFE(),
-    WAVING_OF_FORTUNE(),
+    BULWARK_OF_SIX("Bulwark of Six"),
+    RESISTANCE_OF_SIX("Resistance of Six"),
+    SOULBOUND_POWER_1("Soulbound Power"),
+    SOULBOUND_POWER_2("Soulbound Power"),
+    STRENGTH_OF_NINE("Strength of Nine"),
+    TRIAD_OF_LIFE("Triad of Life"),
+    WAVING_OF_FORTUNE("Waving of Fortune"),
     ;
+
+    private final String name;
+
+    CollectorBagBonusType(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
