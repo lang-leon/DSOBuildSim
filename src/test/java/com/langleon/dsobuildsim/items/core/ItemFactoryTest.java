cb038a0655b7dd2714d5486fe3b66c6b95b27dab
lang-leon <leon.lang1602@gmail.com>
2026-04-01 19:21:41 +0200

    Add test cases for CharacterFactory (replaced CharacterTest with CharacterFactoryTest). Adjusted other test classes for new Character structure.

--- src/test/java/com/langleon/dsobuildsim/items/core/ItemFactoryTest.java
+++ src/test/java/com/langleon/dsobuildsim/items/core/ItemFactoryTest.java
@@ -61,20 +61,14 @@ public class ItemFactoryTest {
     @Test
     void createAnniversaryTwoHandUpgraded() //Unique Item
     {
-        UniqueItem item = itemFactory.createItem(UniqueItemType.ANNIVERSARY_TWO_HAND_UPGRADED, CharacterClass.SPELLWEAVER, Map.of(StatType.DAMAGE, 26326.276, StatType.CRIT_VALUE, 3505.894), 145, Map.of(StatType.ATTACK_SPEED, 0.102), List.of());
+        UniqueItem item = itemFactory.createItem(UniqueItemType.ANNIVERSARY_TWO_HAND_UPGRADED, CharacterClass.SPELLWEAVER, Map.of(StatType.DAMAGE, 26326.276, StatType.CRIT_VALUE, 3505.894), 145, List.of(), List.of(), Map.of(StatType.ATTACK_SPEED, 0.102), List.of());
         Assertions.assertEquals(UniqueItemType.ANNIVERSARY_TWO_HAND_UPGRADED, item.getItemType());
         Assertions.assertEquals(145, item.getLevel());
         Assertions.assertEquals(ItemSlotType.TWO_HAND_WEAPON, item.getItemSlotType());
         Assertions.assertEquals(26326.276, item.getBaseValues().get(StatType.DAMAGE), 0.001);
         Assertions.assertEquals(3505.894, item.getBaseValues().get(StatType.CRIT_VALUE), 0.001);
-        for (int i = 0; i<item.getEnchantments().length; i++)
-        {
-            Assertions.assertNull(item.getEnchantments()[i]);
-        }
-        for (int i = 0; i<item.getGems().length; i++)
-        {
-            Assertions.assertNull(item.getGems()[i]);
-        }
+        Assertions.assertEquals(0, item.getGems().size());
+        Assertions.assertEquals(0, item.getEnchantments().size());
         Assertions.assertEquals(item.getUniqueBaseValues(), Map.of(StatType.ATTACK_SPEED, 0.102));
         Assertions.assertEquals(item.getUniqueRelativeValues(), Map.of(StatType.DAMAGE, 0.10));
         Assertions.assertEquals(item.getUniqueEnchants(), List.of());
@@ -83,21 +77,15 @@ public class ItemFactoryTest {
     @Test
     void createBrigavikGloves() //Unique Item
     {
-        UniqueItem item = itemFactory.createItem(UniqueItemType.BRIGAVIK_GLOVES, CharacterClass.SPELLWEAVER, Map.of(StatType.ARMOR_VALUE, 1067.877, StatType.RESISTANCE_VALUE, 1131.275, StatType.MOVEMENT_SPEED, 0.483), 140, Map.of(), List.of(new Enchantment(StatType.RESISTANCE_VALUE, 0.27198)));
+        UniqueItem item = itemFactory.createItem(UniqueItemType.BRIGAVIK_GLOVES, CharacterClass.SPELLWEAVER, Map.of(StatType.ARMOR_VALUE, 1067.877, StatType.RESISTANCE_VALUE, 1131.275, StatType.MOVEMENT_SPEED, 0.483), 140, List.of(), List.of(), Map.of(), List.of(new Enchantment(StatType.RESISTANCE_VALUE, 0.27198)));
         Assertions.assertEquals(UniqueItemType.BRIGAVIK_GLOVES, item.getItemType());
         Assertions.assertEquals(140, item.getLevel());
         Assertions.assertEquals(ItemSlotType.GLOVES, item.getItemSlotType());
         Assertions.assertEquals(1067.877, item.getBaseValues().get(StatType.ARMOR_VALUE), 0.001);
         Assertions.assertEquals(1131.275, item.getBaseValues().get(StatType.RESISTANCE_VALUE), 0.001);
         Assertions.assertEquals(0.483, item.getBaseValues().get(StatType.MOVEMENT_SPEED), 0.001);
-        for (int i = 0; i<item.getEnchantments().length; i++)
-        {
-            Assertions.assertNull(item.getEnchantments()[i]);
-        }
-        for (int i = 0; i<item.getGems().length; i++)
-        {
-            Assertions.assertNull(item.getGems()[i]);
-        }
+        Assertions.assertEquals(0, item.getGems().size());
+        Assertions.assertEquals(0, item.getEnchantments().size());
         Assertions.assertEquals(Map.of(), item.getUniqueBaseValues());
         Assertions.assertEquals(Map.of(StatType.ARMOR_VALUE, 0.10), item.getUniqueRelativeValues());
         Assertions.assertEquals(List.of(new Enchantment(StatType.RESISTANCE_VALUE, 0.27198)), item.getUniqueEnchants());
@@ -106,21 +94,15 @@ public class ItemFactoryTest {
     @Test
     void createRocketmanAdornment() //Unique Item
     {
-        UniqueItem item = itemFactory.createItem(UniqueItemType.ROCKETMAN_ADORNMENT, CharacterClass.SPELLWEAVER, Map.of(StatType.DAMAGE, 1891.513, StatType.ATTACK_SPEED, 0.055, StatType.CRIT_VALUE, 2103.982), 145, Map.of(), List.of(new Enchantment(StatType.DAMAGE, 0.67637), new Enchantment(StatType.CRIT_VALUE, 0.60126)));
+        UniqueItem item = itemFactory.createItem(UniqueItemType.ROCKETMAN_ADORNMENT, CharacterClass.SPELLWEAVER, Map.of(StatType.DAMAGE, 1891.513, StatType.ATTACK_SPEED, 0.055, StatType.CRIT_VALUE, 2103.982), 145, List.of(), List.of(), Map.of(), List.of(new Enchantment(StatType.DAMAGE, 0.67637), new Enchantment(StatType.CRIT_VALUE, 0.60126)));
         Assertions.assertEquals(UniqueItemType.ROCKETMAN_ADORNMENT, item.getItemType());
-        Assertions.assertEquals(145, item.getLevel());;
+        Assertions.assertEquals(145, item.getLevel());
         Assertions.assertEquals(ItemSlotType.WEAPON_ADORNMENT, item.getItemSlotType());
         Assertions.assertEquals(1891.513, item.getBaseValues().get(StatType.DAMAGE), 0.001);
         Assertions.assertEquals(0.055, item.getBaseValues().get(StatType.ATTACK_SPEED), 0.001);
         Assertions.assertEquals(2103.982, item.getBaseValues().get(StatType.CRIT_VALUE), 0.001);
-        for (int i = 0; i<item.getEnchantments().length; i++)
-        {
-            Assertions.assertNull(item.getEnchantments()[i]);
-        }
-        for (int i = 0; i<item.getGems().length; i++)
-        {
-            Assertions.assertNull(item.getGems()[i]);
-        }
+        Assertions.assertEquals(0, item.getGems().size());
+        Assertions.assertEquals(0, item.getEnchantments().size());
         Assertions.assertEquals(Map.of(), item.getUniqueBaseValues());
         Assertions.assertEquals(Map.of(), item.getUniqueRelativeValues());
         Assertions.assertEquals(List.of(new Enchantment(StatType.DAMAGE, 0.67637), new Enchantment(StatType.CRIT_VALUE, 0.60126)), item.getUniqueEnchants());
@@ -129,21 +111,15 @@ public class ItemFactoryTest {
     @Test
     void createRingOfOldGlory() //Mythic Item
     {
-        MythicItem item = itemFactory.createItem(MythicItemType.RING_OF_OLD_GLORY, CharacterClass.SPELLWEAVER, Map.of(StatType.DAMAGE, 1891.513, StatType.CRIT_VALUE, 1753.505, StatType.HEALTH_POINTS, 24760.80), 145);
+        MythicItem item = itemFactory.createItem(MythicItemType.RING_OF_OLD_GLORY, CharacterClass.SPELLWEAVER, Map.of(StatType.DAMAGE, 1891.513, StatType.CRIT_VALUE, 1753.505, StatType.HEALTH_POINTS, 24760.80), 145, List.of(), List.of());
         Assertions.assertEquals(MythicItemType.RING_OF_OLD_GLORY, item.getItemType());
         Assertions.assertEquals(145, item.getLevel());
         Assertions.assertEquals(ItemSlotType.RING, item.getItemSlotType());
         Assertions.assertEquals(1891.513, item.getBaseValues().get(StatType.DAMAGE), 0.001);
         Assertions.assertEquals(1753.505, item.getBaseValues().get(StatType.CRIT_VALUE), 0.001);
         Assertions.assertEquals(24760.80, item.getBaseValues().get(StatType.HEALTH_POINTS), 0.001);
-        for (int i = 0; i<item.getEnchantments().length; i++)
-        {
-            Assertions.assertNull(item.getEnchantments()[i]);
-        }
-        for (int i = 0; i<item.getGems().length; i++)
-        {
-            Assertions.assertNull(item.getGems()[i]);
-        }
+        Assertions.assertEquals(0, item.getGems().size());
+        Assertions.assertEquals(0, item.getEnchantments().size());
         Assertions.assertEquals(Map.of(), item.getUniqueAbsoluteValues());
         Assertions.assertEquals(Map.of(StatType.DAMAGE, 0.05, StatType.CRIT_VALUE, 0.05), item.getUniqueRelativeValues());
         Assertions.assertEquals(SetType.FORGOTTEN_GLORY, item.getSetType());
@@ -152,21 +128,15 @@ public class ItemFactoryTest {
     @Test
     void createAncestralGloryCloak() //Mythic Item
     {
-        MythicItem item = itemFactory.createItem(MythicItemType.ANCESTRAL_GLORY_CLOAK, CharacterClass.RANGER, Map.of(StatType.DAMAGE, 1891.513, StatType.ATTACK_SPEED, 0.06, StatType.HEALTH_POINTS, 37966.560), 145);
+        MythicItem item = itemFactory.createItem(MythicItemType.ANCESTRAL_GLORY_CLOAK, CharacterClass.RANGER, Map.of(StatType.DAMAGE, 1891.513, StatType.ATTACK_SPEED, 0.06, StatType.HEALTH_POINTS, 37966.560), 145, List.of(), List.of());
         Assertions.assertEquals(MythicItemType.ANCESTRAL_GLORY_CLOAK, item.getItemType());
         Assertions.assertEquals(145, item.getLevel());
         Assertions.assertEquals(ItemSlotType.CLOAK, item.getItemSlotType());
         Assertions.assertEquals(1891.513, item.getBaseValues().get(StatType.DAMAGE), 0.001);
         Assertions.assertEquals(0.06, item.getBaseValues().get(StatType.ATTACK_SPEED), 0.001);
         Assertions.assertEquals(37966.560, item.getBaseValues().get(StatType.HEALTH_POINTS), 0.001);
-        for (int i = 0; i<item.getEnchantments().length; i++)
-        {
-            Assertions.assertNull(item.getEnchantments()[i]);
-        }
-        for (int i = 0; i<item.getGems().length; i++)
-        {
-            Assertions.assertNull(item.getGems()[i]);
-        }
+        Assertions.assertEquals(0, item.getGems().size());
+        Assertions.assertEquals(0, item.getEnchantments().size());
         Assertions.assertEquals(Map.of(StatType.DAMAGE, 5000.0), item.getUniqueAbsoluteValues());
         Assertions.assertEquals(Map.of(StatType.DAMAGE, 0.10), item.getUniqueRelativeValues());
         Assertions.assertEquals(SetType.ANCESTRAL_GLORY, item.getSetType());
@@ -175,70 +145,29 @@ public class ItemFactoryTest {
     @Test
     void createSargonsHorns() //Set Item
     {
-        SetItem item = itemFactory.createItem(SetItemType.SARGON_HELMET, CharacterClass.SPELLWEAVER, Map.of(StatType.DAMAGE, 1644.883, StatType.CRIT_VALUE, 1401.911, StatType.HEALTH_POINTS, 20637.979), 145);
+        SetItem item = itemFactory.createItem(SetItemType.SARGON_HELMET, CharacterClass.SPELLWEAVER, Map.of(StatType.DAMAGE, 1644.883, StatType.CRIT_VALUE, 1401.911, StatType.HEALTH_POINTS, 20637.979), 145, List.of(), List.of());
         Assertions.assertEquals(SetItemType.SARGON_HELMET, item.getItemType());
         Assertions.assertEquals(145, item.getLevel());
         Assertions.assertEquals(ItemSlotType.HELMET, item.getItemSlotType());
         Assertions.assertEquals(1644.883, item.getBaseValues().get(StatType.DAMAGE), 0.001);
         Assertions.assertEquals(1401.911, item.getBaseValues().get(StatType.CRIT_VALUE), 0.001);
         Assertions.assertEquals(20637.979, item.getBaseValues().get(StatType.HEALTH_POINTS), 0.001);
-        for (int i = 0; i<item.getEnchantments().length; i++)
-        {
-            Assertions.assertNull(item.getEnchantments()[i]);
-        }
-        for (int i = 0; i<item.getGems().length; i++)
-        {
-            Assertions.assertNull(item.getGems()[i]);
-        }
+        Assertions.assertEquals(0, item.getGems().size());
+        Assertions.assertEquals(0, item.getEnchantments().size());
         Assertions.assertEquals(SetType.SARGONS_IMMORTAL_TERROR_ARMOR, item.getSetType());
     }
 
     @Test
     void createBigPawsTwoHand() //Set Item
     {
-        SetItem item = itemFactory.createItem(SetItemType.BIG_PAWS_TWO_HAND, CharacterClass.SPELLWEAVER, Map.of(StatType.DAMAGE, 13163.139, StatType.CRIT_VALUE, 3505.893), 145);
+        SetItem item = itemFactory.createItem(SetItemType.BIG_PAWS_TWO_HAND, CharacterClass.SPELLWEAVER, Map.of(StatType.DAMAGE, 13163.139, StatType.CRIT_VALUE, 3505.893), 145, List.of(), List.of());
         Assertions.assertEquals(SetItemType.BIG_PAWS_TWO_HAND, item.getItemType());
         Assertions.assertEquals(145, item.getLevel());
         Assertions.assertEquals(ItemSlotType.TWO_HAND_WEAPON, item.getItemSlotType());
         Assertions.assertEquals(13163.139, item.getBaseValues().get(StatType.DAMAGE), 0.001);
         Assertions.assertEquals(3505.893, item.getBaseValues().get(StatType.CRIT_VALUE), 0.001);
-        for (int i = 0; i<item.getEnchantments().length; i++)
-        {
-            Assertions.assertNull(item.getEnchantments()[i]);
-        }
-        for (int i = 0; i<item.getGems().length; i++)
-        {
-            Assertions.assertNull(item.getGems()[i]);
-        }
+        Assertions.assertEquals(0, item.getGems().size());
+        Assertions.assertEquals(0, item.getEnchantments().size());
         Assertions.assertEquals(SetType.BIG_PAWS_FROSTY_GREED, item.getSetType());
-    }
-
-
-    @Test
-    void createAndUpdateItemBaseStats()
-    {
-        SetItem item = itemFactory.createItem(SetItemType.SARGON_HELMET, CharacterClass.SPELLWEAVER, Map.of(StatType.DAMAGE, 1644.883, StatType.CRIT_VALUE, 1401.911, StatType.HEALTH_POINTS, 20637.979), 145);
-        Assertions.assertEquals(1644.883, item.getBaseValues().get(StatType.DAMAGE), 0.001);
-        Assertions.assertEquals(1401.911, item.getBaseValues().get(StatType.CRIT_VALUE), 0.001);
-        Assertions.assertEquals(20637.979, item.getBaseValues().get(StatType.HEALTH_POINTS), 0.001);
-        Map<StatType, Double> updatedValues = Map.of(StatType.DAMAGE, 1500.0, StatType.CRIT_VALUE, 1350.0);
-        item.updateBaseValues(updatedValues);
-        Assertions.assertEquals(1500.0, item.getBaseValues().get(StatType.DAMAGE), 0.001);
-        Assertions.assertEquals(1350.0, item.getBaseValues().get(StatType.CRIT_VALUE), 0.001);
-        Assertions.assertEquals(20637.979, item.getBaseValues().get(StatType.HEALTH_POINTS), 0.001);
-    }
-
-    @Test
-    void shouldThrowOnUpdateWithInvalidKeys()
-    {
-        SetItem item = itemFactory.createItem(SetItemType.SARGON_HELMET, CharacterClass.SPELLWEAVER, Map.of(StatType.DAMAGE, 1644.883, StatType.CRIT_VALUE, 1401.911, StatType.HEALTH_POINTS, 20637.979), 145);
-        Assertions.assertEquals(1644.883, item.getBaseValues().get(StatType.DAMAGE), 0.001);
-        Assertions.assertEquals(1401.911, item.getBaseValues().get(StatType.CRIT_VALUE), 0.001);
-        Assertions.assertEquals(20637.979, item.getBaseValues().get(StatType.HEALTH_POINTS), 0.001);
-        Map<StatType, Double> updatedValues = Map.of(StatType.DAMAGE, 1500.0, StatType.MANA, 13.0, StatType.MOVEMENT_SPEED, 0.5);
-        Assertions.assertThrows(IllegalArgumentException.class, () -> item.updateBaseValues(updatedValues));
-        Assertions.assertEquals(1644.883, item.getBaseValues().get(StatType.DAMAGE), 0.001);
-        Assertions.assertEquals(1401.911, item.getBaseValues().get(StatType.CRIT_VALUE), 0.001);
-        Assertions.assertEquals(20637.979, item.getBaseValues().get(StatType.HEALTH_POINTS), 0.001);
     }
 }
