c1a7372363a6db659a0c77075f054a7f9ecd4e50
lang-leon <leon.lang1602@gmail.com>
2026-04-04 23:21:58 +0200

    Removes unused and obsolete files from the codebase to clean up the project structure.

--- src/main/java/com/langleon/dsobuildsim/items/setitems/SetItemConfig.java
+++
@@ -1,7 +0,0 @@
-package com.langleon.dsobuildsim.items.setitems;
-
-import java.util.Map;
-
-public record SetItemConfig (Map<SetItemType, SetItemDefinition> spellweaverItems, Map<SetItemType, SetItemDefinition> dragonknightItems,
-                             Map<SetItemType, SetItemDefinition> rangerItems, Map<SetItemType, SetItemDefinition> steamMechanicusItems){
-}
