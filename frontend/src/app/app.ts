29a87532ce4c9d876ce182f563e355922947d646
lang-leon <leon.lang1602@gmail.com>
2026-07-17 19:54:16 +0200

    Add frontend character JSON upload and stats calculation flow. Adjusted website icon and title.

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CharacterComponent } from './components/character/character-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CharacterComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('frontend');
+  
 }
