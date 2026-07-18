53172307c82456cd54c4a376d6ad16c242bb0157
lang-leon <leon.lang1602@gmail.com>
2026-07-18 15:01:37 +0200

    Add stats display to display character default stats and update once new stats are calculated.

--- frontend/src/app/components/BuildSim/build-sim-component.ts
+++ frontend/src/app/components/BuildSim/build-sim-component.ts
@@ -1,13 +1,16 @@
 import { ChangeDetectorRef, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { StatCalculationService } from '../../services/stat-calculation-service';
-import { StatsDTO } from '../../models/stats-dto';
 import { GameDataDTO } from '../../models/game-data-dto';
 import { GameDataService } from '../../services/game-data-service';
+import { StatType } from '../../models/stat-type';
+import { ClassStatsDTO } from '../../models/class-stats-dto';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-character',
   standalone: true,
   imports: [
+    CommonModule
   ],
   templateUrl: './build-sim-component.html',
   styleUrl: './build-sim-component.scss',
@@ -18,7 +21,9 @@ export class BuildSimComponent implements OnInit{
   @ViewChild('fileInput')
   fileInput!: ElementRef<HTMLInputElement>;
 
+  StatType = StatType;
   gameData?: GameDataDTO;
+  stats?: ClassStatsDTO;
 
   constructor(
     private statCalculationService: StatCalculationService,
@@ -30,7 +35,9 @@ export class BuildSimComponent implements OnInit{
     this.gameDataService.getGameData()
     .subscribe(data => {
       this.gameData = data;
-      console.log(this.gameData);
+      this.stats = this.gameData.characterClassStats["SPELLWEAVER"];
+      console.log(this.stats);
+      this.changeDetector.detectChanges();
     });
   }
 
@@ -63,7 +70,14 @@ export class BuildSimComponent implements OnInit{
   calculate(character: object)
   {
     this.statCalculationService.calculateStats(character).subscribe((response: any) => {
+      this.stats!.absoluteStats = response.stats;
+      this.changeDetector.detectChanges();
+
+      console.log(this.gameData);
+
       console.log(response);
+      console.log("stats:");
+      console.log(this.stats?.absoluteStats);
     });
   }
 }
