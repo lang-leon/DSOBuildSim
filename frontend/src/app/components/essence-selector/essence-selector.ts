a4b11051ac1b0e41d6a6ba17c6ffc85bfd28c10f
lang-leon <leon.lang1602@gmail.com>
2026-09-18 16:23:41 +0200

    format essence selector files move common styling into styles.scs

--- frontend/src/app/components/essence-selector/essence-selector.ts
+++ frontend/src/app/components/essence-selector/essence-selector.ts
@@ -8,16 +8,13 @@ import { StatType } from '../../enums/StatType';
 
 @Component({
   selector: 'app-essence-selector',
-  imports: [
-    FormsModule
-  ],
+  imports: [FormsModule],
   templateUrl: './essence-selector.html',
   styleUrl: './essence-selector.scss',
 })
 export class EssenceSelector {
-  
   @Input() scale = 1;
-  
+
   @Input() essences!: EssenceDefinitionDTO[];
 
   @Input() character!: CharacterDTO;
@@ -38,17 +35,13 @@ export class EssenceSelector {
 
   ngOnInit() {
     if (this.character.essence) {
-        this.selectedEssence =
-            this.essences.find(
-                essence => essence.essenceType === this.character.essence!.essenceType
-            ) ?? null;
+      this.selectedEssence =
+        this.essences.find(
+          (essence) => essence.essenceType === this.character.essence!.essenceType,
+        ) ?? null;
 
-        this.selectedTier = this.character.essence.tier;
+      this.selectedTier = this.character.essence.tier;
     }
-}
-
-  get essenceTypes(): string[] {
-    return [...new Set(this.essences.map((essence) => essence.essenceType))];
   }
 
   get availableTiers(): number[] {
@@ -70,17 +63,17 @@ export class EssenceSelector {
     this.cancelled.emit();
   }
 
-confirm() {
+  confirm() {
     if (!this.selectedEssence) {
-        this.confirmed.emit(null);
-        return;
+      this.confirmed.emit(null);
+      return;
     }
 
     const essence: EssenceInstanceDTO = {
-        essenceType: this.selectedEssence.essenceType,
-        tier: this.selectedTier
+      essenceType: this.selectedEssence.essenceType,
+      tier: this.selectedTier,
     };
 
     this.confirmed.emit(essence);
-}
+  }
 }
